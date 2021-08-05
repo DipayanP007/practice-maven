be07032a012e3cc08230768f0380c0afe50ca141
Dipayan Pramanik <dipayan.pramanik@knoldus.com>
2021-08-05 19:58:12 +0530

    initial commit

---
+++ calApi/src/main/java/org/calc/Func.java
@@ -0,0 +1,9 @@
+package org.calc;
+
+public class Func {
+
+    public int add(int a,int b)
+    {
+        return (a+b);
+    }
+}
---
+++ calImpl/src/main/java/org/calc/mainfunc/Main.java
@@ -0,0 +1,12 @@
+package org.calc.mainfunc;
+
+import org.calc.Func;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Func ob = new Func();
+        System.out.println(ob.add(5,3));
+    }
+}
